037978ad716555aa2fea5c84e0015a7faf64aaea
Ruthwik182003 <ruthvikramesh18@gmail.com>
2025-08-15 00:51:15 +0530

    Initial commit

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,12 +5,14 @@ import App from './App.jsx';
 import './styles.css';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-      <ToastContainer position="top-center" autoClose={2000} />
-    </BrowserRouter>
-  </React.StrictMode>
+  <HashRouter>
+    <App />
+  </HashRouter>
 );
+
--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: 'https://Ruthwik182003.github.io/react_auth_form', // <-- replace with your repo name
-})
-
+  base: '/react_auth_forms/', // repo name here
+});
