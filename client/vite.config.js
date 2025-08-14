dde041ea26adb95f61c705b2772fe93148f21b9d
Ruthwik182003 <ruthvikramesh18@gmail.com>
2025-08-15 00:29:34 +0530

    Updated for github

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173
-  }
-});
+  base: 'https://Ruthwik182003.github.io/react_auth_form', // <-- replace with your repo name
+})
+
