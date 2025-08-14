c38840cfd282a20aab2686daca052f678f163044
Ruthwik182003 <ruthvikramesh18@gmail.com>
2025-08-15 00:57:16 +0530

    initial commit

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,18 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App.jsx';
-import './styles.css';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
 import App from './App';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <HashRouter>
     <App />
+    <ToastContainer />
   </HashRouter>
 );
-
